563ae36e965266e5ceef9f2c01a94ec8c715c7e9
Mateusz Cegiełka <mateusz@cegla.net>
2023-02-26 19:28:22 +0100

    Add creating swapchain

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,14 @@
 #![feature(const_cstr_methods)]
 
 use ash::extensions::ext::DebugUtils;
-use ash::extensions::khr::Surface;
-use ash::{vk, Entry};
+use ash::extensions::khr::{Surface, Swapchain};
+use ash::prelude::VkResult;
+use ash::{vk, Entry, Instance};
 use raw_window_handle::{HasRawDisplayHandle, HasRawWindowHandle};
 use std::borrow::Cow;
 use std::collections::HashSet;
 use std::ffi::CStr;
+use std::mem::swap;
 use winit::event::{Event, WindowEvent};
 use winit::event_loop::EventLoop;
 use winit::platform::run_return::EventLoopExtRunReturn;
@@ -19,6 +21,123 @@ const VULKAN_APP_NAME: &CStr = unsafe { CStr::from_bytes_with_nul_unchecked(b"Vu
 const VULKAN_APP_VERSION: u32 = vk::make_api_version(0, 0, 0, 0);
 const VULKAN_ENGINE_NAME: &CStr = unsafe { CStr::from_bytes_with_nul_unchecked(b"Vulkthing\0") };
 const VULKAN_ENGINE_VERSION: u32 = vk::make_api_version(0, 0, 0, 0);
+
+struct QueueDetails {
+    graphics_family: u32,
+    present_family: u32,
+}
+
+struct SwapchainDetails {
+    capabilities: vk::SurfaceCapabilitiesKHR,
+    formats: Vec<vk::SurfaceFormatKHR>,
+    present_modes: Vec<vk::PresentModeKHR>,
+}
+
+impl QueueDetails {
+    fn query(
+        instance: &Instance,
+        device: vk::PhysicalDevice,
+        surface: &Surface,
+        surface_khr: vk::SurfaceKHR,
+    ) -> VkResult<Option<QueueDetails>> {
+        let queues = unsafe { instance.get_physical_device_queue_family_properties(device) };
+        let Some(graphics_family) = QueueDetails::find_queue(&queues, |_, q| q.queue_flags.contains(vk::QueueFlags::GRAPHICS)) else {
+            return Ok(None);
+        };
+        let Some(present_family) = QueueDetails::find_queue(&queues, |i, _| unsafe { surface.get_physical_device_surface_support(device, i, surface_khr) }
+            .unwrap()) else {
+            return Ok(None);
+        };
+        Ok(Some(QueueDetails {
+            graphics_family,
+            present_family,
+        }))
+    }
+
+    fn find_queue(
+        queues: &[vk::QueueFamilyProperties],
+        p: impl Fn(u32, &vk::QueueFamilyProperties) -> bool,
+    ) -> Option<u32> {
+        for (index, queue) in queues.iter().enumerate() {
+            let index = index as u32;
+            if p(index, queue) {
+                return Some(index);
+            }
+        }
+        None
+    }
+}
+
+impl SwapchainDetails {
+    fn query(
+        device: vk::PhysicalDevice,
+        surface: &Surface,
+        surface_khr: vk::SurfaceKHR,
+    ) -> VkResult<SwapchainDetails> {
+        let capabilities =
+            unsafe { surface.get_physical_device_surface_capabilities(device, surface_khr) }
+                .unwrap();
+        let formats =
+            unsafe { surface.get_physical_device_surface_formats(device, surface_khr) }.unwrap();
+        let present_modes =
+            unsafe { surface.get_physical_device_surface_present_modes(device, surface_khr) }
+                .unwrap();
+        Ok(SwapchainDetails {
+            capabilities,
+            formats,
+            present_modes,
+        })
+    }
+
+    fn select_format(&self) -> vk::SurfaceFormatKHR {
+        self.formats
+            .iter()
+            .find(|f| {
+                f.format == vk::Format::B8G8R8A8_SRGB
+                    && f.color_space == vk::ColorSpaceKHR::SRGB_NONLINEAR
+            })
+            .unwrap_or(&self.formats[0])
+            .clone()
+    }
+
+    fn select_present_mode(&self) -> vk::PresentModeKHR {
+        self.present_modes
+            .iter()
+            .copied()
+            .find(|m| *m == vk::PresentModeKHR::MAILBOX)
+            .unwrap_or(vk::PresentModeKHR::FIFO)
+    }
+
+    fn select_swap_extent(&self, window: &winit::window::Window) -> vk::Extent2D {
+        if self.capabilities.current_extent.width != u32::MAX {
+            return self.capabilities.current_extent;
+        }
+        let window_size = window.inner_size();
+        vk::Extent2D {
+            width: window_size.width.clamp(
+                self.capabilities.min_image_extent.width,
+                self.capabilities.max_image_extent.width,
+            ),
+            height: window_size.height.clamp(
+                self.capabilities.min_image_extent.height,
+                self.capabilities.max_image_extent.height,
+            ),
+        }
+    }
+
+    fn select_image_count(&self) -> u32 {
+        let no_image_limit = self.capabilities.max_image_count == 0;
+        dbg!(no_image_limit);
+        self.capabilities.min_image_count
+            + if no_image_limit
+                || self.capabilities.min_image_count + 1 <= self.capabilities.max_image_count
+            {
+                1
+            } else {
+                0
+            }
+    }
+}
 
 fn main() {
     // Create the application window using winit. Use a predefined size for now, though games should
@@ -85,7 +204,7 @@ fn main() {
 
     // Create the KHR extension surface from winit object. This must be done before selecting a
     // physical device to check whether is supports presenting to the display.
-    let surface = unsafe {
+    let surface_khr = unsafe {
         ash_window::create_surface(
             &entry,
             &instance,
@@ -95,7 +214,7 @@ fn main() {
         )
     }
     .unwrap();
-    let surface_loader = Surface::new(&entry, &instance);
+    let surface = Surface::new(&entry, &instance);
 
     // Select the GPU. For now, just select the first discrete GPU with graphics support. Later,
     // this should react better to iGPU, dGPU and iGPU+dGPU setups. In more complex setups, it would
@@ -107,48 +226,39 @@ fn main() {
         let gpu_name = unsafe { CStr::from_ptr(properties.device_name.as_ptr()) }
             .to_str()
             .unwrap();
-        let queues = unsafe { instance.get_physical_device_queue_family_properties(device) };
-        let is_discrete_gpu = properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU;
-        let has_graphics_queue = queues
-            .iter()
-            .any(|queue| queue.queue_flags.contains(vk::QueueFlags::GRAPHICS));
-        let has_present_queue = (0..queues.len() as u32).into_iter().any(|i| {
-            unsafe { surface_loader.get_physical_device_surface_support(device, i, surface) }
-                .unwrap()
+        let Some(queues) = QueueDetails::query(&instance, device, &surface, surface_khr).unwrap() else {
+            println!("rejected gpu, no suitable queues ({gpu_name})");
+            continue;
+        };
+        let extensions = unsafe { instance.enumerate_device_extension_properties(device) }.unwrap();
+        let has_swapchain_extension = extensions.iter().any(|ext| {
+            let ext_name = unsafe { CStr::from_ptr(ext.extension_name.as_ptr()) };
+            ext_name == Swapchain::name()
         });
-        dbg!(
-            gpu_name,
-            is_discrete_gpu,
-            has_graphics_queue,
-            has_present_queue
-        );
-        if is_discrete_gpu && has_graphics_queue && has_present_queue && found.is_none() {
-            println!("selected gpu: {gpu_name}");
-            found = Some((device, queues));
+        if !has_swapchain_extension {
+            println!("rejected gpu, no swapchain extension ({gpu_name})");
+            continue;
+        }
+        // This query requires the swapchain extension to be present.
+        let swapchains = SwapchainDetails::query(device, &surface, surface_khr).unwrap();
+        if swapchains.formats.is_empty() || swapchains.present_modes.is_empty() {
+            println!("rejected gpu, unsuitable swapchain ({gpu_name})");
+            continue;
+        }
+        if found.is_none() {
+            println!("accepted gpu: {gpu_name}");
+            found = Some((device, queues, swapchains));
+        } else {
+            println!("rejected gpu, one already selected ({gpu_name})");
         }
     }
-    let Some((physical_device, queues)) = found else {
+    let Some((physical_device, queues, swapchains)) = found else {
         panic!("gpu not found");
     };
 
     // Specify physical device extensions, required queues and create them. Probably should pick
     // queues more reasonably?
-    let graphics_queue_index = queues
-        .iter()
-        .enumerate()
-        .find(|(_, queue)| queue.queue_flags.contains(vk::QueueFlags::GRAPHICS))
-        .unwrap()
-        .0 as u32;
-    let present_queue_index = (0..queues.len() as u32)
-        .into_iter()
-        .find(|i| {
-            unsafe {
-                surface_loader.get_physical_device_surface_support(physical_device, *i, surface)
-            }
-            .unwrap()
-        })
-        .unwrap();
-    let queue_indices = HashSet::from([graphics_queue_index, present_queue_index]);
+    let queue_indices = HashSet::from([queues.graphics_family, queues.present_family]);
     let queue_creates: Vec<_> = queue_indices
         .iter()
         .map(|queue_index| {
@@ -165,13 +275,49 @@ fn main() {
             &vk::DeviceCreateInfo::builder()
                 .queue_create_infos(&queue_creates)
                 .enabled_features(&physical_device_features)
-                .enabled_layer_names(&layer_names),
+                .enabled_layer_names(&layer_names)
+                .enabled_extension_names(&[Swapchain::name().as_ptr()]),
             None,
         )
     }
     .unwrap();
-    let graphics_queue = unsafe { device.get_device_queue(graphics_queue_index, 0) };
-    let present_queue = unsafe { device.get_device_queue(present_queue_index, 0) };
+    let graphics_queue = unsafe { device.get_device_queue(queues.graphics_family, 0) };
+    let present_queue = unsafe { device.get_device_queue(queues.present_family, 0) };
+
+    // Create the swapchain for presenting images to display. Set to prefer triple buffering right
+    // now, should be possible to change on laptops or integrated GPUs?
+    let format = swapchains.select_format();
+    let present_mode = swapchains.select_present_mode();
+    let extent = swapchains.select_swap_extent(&window);
+    let image_count = swapchains.select_image_count();
+    let swapchain_create_info = vk::SwapchainCreateInfoKHR::builder()
+        .surface(surface_khr)
+        .min_image_count(image_count)
+        .image_format(format.format)
+        .image_color_space(format.color_space)
+        .image_extent(extent)
+        .image_array_layers(1)
+        .image_usage(vk::ImageUsageFlags::COLOR_ATTACHMENT);
+    let queue_family_indices = [queues.graphics_family, queues.present_family];
+    let swapchain_create_info = if queues.graphics_family != queues.present_family {
+        swapchain_create_info
+            .image_sharing_mode(vk::SharingMode::CONCURRENT)
+            .queue_family_indices(&queue_family_indices)
+    } else {
+        swapchain_create_info.image_sharing_mode(vk::SharingMode::EXCLUSIVE)
+    };
+    let swapchain_create_info = swapchain_create_info
+        .pre_transform(swapchains.capabilities.current_transform)
+        .composite_alpha(vk::CompositeAlphaFlagsKHR::OPAQUE)
+        .present_mode(present_mode)
+        .clipped(true)
+        .old_swapchain(vk::SwapchainKHR::null());
+    let swapchain = Swapchain::new(&instance, &device);
+    let swapchain_khr =
+        unsafe { swapchain.create_swapchain(&swapchain_create_info, None) }.unwrap();
+    let swapchain_images = unsafe { swapchain.get_swapchain_images(swapchain_khr) }.unwrap();
+    let swapchain_image_format = format.format;
+    let swapchain_extent = extent;
 
     // Run the event loop. Winit delivers events, like key presses. After it finishes delivering
     // some batch of events, it sends a MainEventsCleared event, which means the application should
@@ -195,8 +341,9 @@ fn main() {
         }
     });
 
+    unsafe { swapchain.destroy_swapchain(swapchain_khr, None) };
     unsafe { device.destroy_device(None) };
-    unsafe { surface_loader.destroy_surface(surface, None) };
+    unsafe { surface.destroy_surface(surface_khr, None) };
     unsafe { debug_utils_loader.destroy_debug_utils_messenger(debug_call_back, None) };
     unsafe { instance.destroy_instance(None) };
 }
